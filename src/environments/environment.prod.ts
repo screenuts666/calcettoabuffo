2a68d2c1a15eb2b47d9d5ad796f183485100be62
screenuts <screenuts@gmail.com>
2026-03-03 04:27:21 +0100

    update maximun

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCqnjBXeJoJ7ZfUTLMYyAMBNhBZiazA7mA",
+    authDomain: "calcettoabbuffone.firebaseapp.com",
+    projectId: "calcettoabbuffone",
+    storageBucket: "calcettoabbuffone.firebasestorage.app",
+    messagingSenderId: "1048869516820",
+    appId: "1:1048869516820:web:02ca970bf5fafd47f79901"
+  }
 };
