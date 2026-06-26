167e2fd4da12ab29cb4313c3e5477f120032e649
screenuts <screenuts@gmail.com>
2026-06-26 11:44:42 +0200

    feat: ottimizzazione avvio PWA, aggiunta loader per schermata nera e correzione script deploy dev

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ bootstrapApplication(AppComponent, {
     provideAuth(() => getAuth()),
     provideZonelessChangeDetection(), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
+            registrationStrategy: 'registerImmediately'
           }),
   ],
 });
